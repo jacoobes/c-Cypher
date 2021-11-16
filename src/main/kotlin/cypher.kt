f24ecd7f69c85f21e3257a968f6b0efc0e60edde
jacoobes <jacobnguyend@gmail.com>
2021-11-16 13:27:18 -0600

    abstraction, more accurate de/en cryption

--- src/main/kotlin/cypher.kt
+++ src/main/kotlin/cypher.kt
@@ -2,19 +2,31 @@ import java.util.*
 
 class CCypher(private val shift : Int) {
     private val asciiLetters = ('A'..'Z')
+    private val otherChars = setOf('.', '?', '!', '*', ' ') to setOf('.', '?', '!', '*', ' ')
     fun encode(value: String) : String {
         val cypher = value.uppercase(Locale.getDefault()).toList()
-        return cypher.map { cAlphabet(shift)[it] ?: ' ' }.joinToString("")
+        return cypher.map { translate(shift, it) }.joinToString("")
     }
     fun decode(value: String) : String {
         val cypher = value.uppercase(Locale.getDefault()).toList()
-        return cypher.map { cAlphabet(-shift)[it] ?: ' '}.joinToString("")
+        return cypher.map {translate(-shift, it) }.joinToString("")
     }
     private fun values(shift: Int): List<Char> {
         val list = asciiLetters.toList()
         Collections.rotate(list, shift)
         return list
     }
-    private fun cAlphabet(rotate: Int) : Map<Char, Char> = values(rotate).zip(asciiLetters).toMap()
+
+    private fun translate(rotate: Int, char: Char) =
+        when (char) {
+            '.', '?', '!', '*', ' ' -> otherChars[char]!!
+            else -> cAlphabet(rotate)[char]!!
+        }
+
+    private infix fun <T,V> Iterable<T>.to(other: Iterable<V>) : Map<T, V> {
+        return this.zip(other).toMap()
+    }
+
+    private fun cAlphabet(rotate: Int) : Map<Char, Char> = values(rotate) to asciiLetters
 }
 
